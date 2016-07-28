f96b988d324e3ed4d53533829fccb78a10bb6bc9
Jon Manning <jon@secretlab.com.au>
2016-07-28 19:29:45 +1000

    Nicer commenting

--- abc-dl.py
+++ abc-dl.py
@@ -79,6 +79,7 @@ SKIP_IDS = []
 
 def main():
     
+    # parse command line options
     parser = OptionParser()
     
     parser.add_option("-o", "--output-dir", action="store", default="output", help="The destination folder. Will be created if it doesn't exist. Default: output/")
@@ -91,78 +92,93 @@ def main():
     
     parser.add_option("-s", "--slowdown-factor", default=0.5, type=float, help="The time (in seconds) between requests when throttling.")
     
-    
     (options, args) = parser.parse_args()
     
     if len(args) < 2:
         print("Usage: abc-dl.py <from id> <to id>\n\tFor more information, run abc-dl.py --help")
         return
     
+    # set up the logger
+    logging.basicConfig(format='%(asctime)s %(message)s', filename=options.log, level=logging.INFO)
+    
+    # make the output directory if it doesn't exist
     if os.path.isdir(options.output_dir) == False:        
         os.makedirs(options.output_dir)
-        print("Creating output directory {}".format(options.output_dir))
-        
-    logging.basicConfig(format='%(asctime)s %(message)s', filename=options.log, level=logging.INFO)
-        
+        logging.info("Creating output directory {}".format(options.output_dir))
     
+    # we'll be requesting stuff within this range
     id_range = range(int(args[0]), int(args[1]))
     
-    
+    # we'll skip all requests whose ID is in this list
     SKIP_IDS = []
     
+    # does the error file exist? read all IDs found in it
     if os.path.isfile(options.errors_file):
         SKIP_IDS = [int(line) for line in open(options.errors_file).readlines()]
     
+    # we'll append to the skip file
     skip_file = open(options.errors_file, "a")
     
+    # does the index file exist? read all IDs found in it, too
     if os.path.isfile(options.index_file):
         SKIP_IDS += [int(line) for line in open(options.index_file).readlines()]
-        
-    SKIP_IDS = set(SKIP_IDS)
     
+    # and open the index file so we can append to it
     index_file = open(options.index_file, "a")
     
+    # make this a set to make searching faster
+    SKIP_IDS = set(SKIP_IDS)
+    
+    # show a pretty progress bar
     pbar = ProgressBar(widgets=[AdaptiveETA(), Percentage(), Bar(), SimpleProgress()], maxval=len(id_range)).start()
     
-    
+    # this var contains the time after which we'll resume normal speed; 
+    # it starts in the past
     stop_delaying_at = datetime.now() - timedelta(seconds=5)
     
-    
+    # let's a-go!
     for i, article_id in enumerate(id_range):
         
         # skip odd-numbered IDs, which are always folders
         if article_id % 2 != 0:
             continue
         
+        # skip articles that have either errored or downloaded already
         if article_id in SKIP_IDS:
             continue
-            
-        now = datetime.now()
         
-        if now < stop_delaying_at:
+        # wait a bit before doing this request, if necessary
+        if datetime.now() < stop_delaying_at:
             time.sleep(options.slowdown_factor)
         
+        # get the resource
         url = URL_TEMPLATE.format(article_id)
-        
-        #print("Getting {}".format(url))
         
         result = requests.get(url)
         
+        # update the progress bar
         pbar.update(i)
         
+        # did the server tell us to slow down?
         if result.status_code == requests.codes.too_many_requests:
             
+            # indicate that we should stop delaying in 10 minutes
             stop_delaying_at = datetime.now() + timedelta(minutes = 10)
             
             logging.info("Server indicated too many requests; slowing for 10 minutes (will resume full speed at {})".format(stop_delaying_at))
             
+            # skip this resource, which is probably OK
+            # TODO: wait a moment and try this request again
             continue
             
-        
+        # was it successful?
         if result.status_code == requests.codes.ok:
             
+            # attempt to parse the json and determine if it's worth keeping
             try:
                 if "docType" in result.json():
+                    
+                    # it's a valid resource, save it
                     filename = "{}_{}.json".format(result.json()["docType"], article_id)
             
                     output_path = os.path.join(options.output_dir, filename)
@@ -175,23 +191,30 @@ def main():
                     if options.index_file:
                         index_file.write("{}\n".format(article_id))
                 else:
+                    
+                    # it's not a valid document
                     logging.info("Skipping {} (HTTP 200 but not a valid document)".format(url))
                     if options.errors_file:
                         skip_file.write("{}\n".format(article_id))
                         
             except ValueError as e:
+                # we couldn't parse the JSON
+                
                 logging.info("Skipping {} (invalid JSON)".format(url))
+                
+                # record that this is a bad entry
                 if options.errors_file:
                     skip_file.write("{}\n".format(article_id))
-            
-            
-            
         else:
+            # we got some other HTTP code, so skip it and record that it's bad
             logging.info("Skipping {} (HTTP {})".format(url, result.status_code))
             if options.errors_file:
                 skip_file.write("{}\n".format(article_id))
 
+    # all done!
     pbar.finish()
+    
+    logging.info("Process complete!")
 
 if __name__ == '__main__':
     main()
